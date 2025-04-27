810620e43cad91ee08cec84aef3a3c22bb41bd69
Paradoxe Ngwasi <paradoxngwasi@gmail.com>
2025-04-27 22:03:53 +0000

    added medusa lib files

---
+++ apps/storefront/src/components/icons/credit-card.tsx
@@ -0,0 +1,20 @@
+export default function CreditCard() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="none">
+      <g
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="1.5"
+        clip-path="url(#a)"
+      >
+        <path d="M1.056 5.944h12.888M2.833 12.167h9.334c.982 0 1.777-.796 1.777-1.778V4.61c0-.982-.796-1.778-1.777-1.778H2.833c-.981 0-1.777.796-1.777 1.778v5.778c0 .982.796 1.778 1.777 1.778M3.278 9.5h2.666M10.833 9.5h.89"></path>
+      </g>
+      <defs>
+        <clipPath id="a">
+          <path fill="#fff" d="M0 0h15v15H0z"></path>
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
--- apps/storefront/src/lib/constants.ts
+++
@@ -1,2 +0,0 @@
-export const BRAND_NAME = "Grace & Carry";
-export const SUPPORT_CONTACT = "support@gracecarry.shop";
